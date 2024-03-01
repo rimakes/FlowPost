a0532e885bb2ca7e4480319e07b1b4df6eda5680
jayraj <jayraj@secretmindtech.com>
2024-03-01 19:19:55 +0530

    schedule api and cron job

---
+++ app/api/schedule-post/route.ts
@@ -0,0 +1,164 @@
+import axios from 'axios'
+import schedule from 'node-schedule'
+import { NextRequest, NextResponse } from 'next/server'
+import { db } from '@/lib/prisma'
+import https from 'https'
+
+const currentTime = new Date()
+
+const scheduledPosts = [
+  {
+    content: 'Post on 1',
+    time: currentTime.setMinutes(currentTime.getMinutes()),
+  },
+  {
+    content: 'Post on 2',
+    time: currentTime.setMinutes(currentTime.getMinutes()),
+  },
+  {
+    content: 'Post on 3',
+    time: currentTime.setMinutes(currentTime.getMinutes()),
+  },
+  {
+    content: 'Post on 4',
+    time: currentTime.setMinutes(currentTime.getMinutes()),
+  },
+]
+
+console.log(scheduledPosts, '====scheduledPosts')
+
+// const postOnLinkedIn = async (
+//   providerAccountId: String,
+//   content: String,
+//   accessToken: String | null
+// ) => {
+//   try {
+//     const body = {
+//       author: `urn:li:person:${providerAccountId}`,
+//       lifecycleState: 'PUBLISHED',
+//       specificContent: {
+//         'com.linkedin.ugc.ShareContent': {
+//           shareCommentary: {
+//             text: content,
+//           },
+//           shareMediaCategory: 'NONE',
+//         },
+//       },
+//       visibility: {
+//         'com.linkedin.ugc.MemberNetworkVisibility': 'PUBLIC',
+//       },
+//     }
+
+//     const url = 'https://api.linkedin.com/v2/ugcPosts'
+//     const headers = {
+//       Authorization: `Bearer ${accessToken}`,
+//       'Content-Type': 'application/json',
+//     }
+
+//     const response = await axios.post(url, body, {
+//       headers,
+//       timeout: 5000,
+//       httpAgent: new https.Agent({
+//         keepAlive: true,
+//         scheduling: 'fifo',
+//       }),
+//     })
+
+//     console.log('Post successfully posted on LinkedIn:', response?.data?.id)
+//   } catch (error: any) {
+//     console.error('Error posting on LinkedIn:', error)
+//   }
+// }
+
+const postOnLinkedIn = async (
+  providerAccountId: String,
+  content: String,
+  accessToken: String | null
+) => {
+  try {
+    const body = {
+      author: `urn:li:person:${providerAccountId}`,
+      lifecycleState: 'PUBLISHED',
+      specificContent: {
+        'com.linkedin.ugc.ShareContent': {
+          shareCommentary: {
+            text: content,
+          },
+          shareMediaCategory: 'NONE',
+        },
+      },
+      visibility: {
+        'com.linkedin.ugc.MemberNetworkVisibility': 'PUBLIC',
+      },
+    }
+
+    const url = 'https://api.linkedin.com/v2/ugcPosts'
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    }
+
+    const axiosInstance = axios.create({
+      timeout: 10000,
+      httpAgent: new https.Agent({
+        keepAlive: true,
+        scheduling: 'fifo',
+      }),
+    })
+
+    const response = await axiosInstance.post(url, body, { headers })
+
+    console.log('Post successfully posted on LinkedIn:', response?.data?.id)
+  } catch (error: any) {
+    console.error('Error posting on LinkedIn:', error)
+
+    if (error.response && error.response.status >= 500) {
+      // Retry logic for server errors
+      console.log('Retrying post...')
+      await postOnLinkedIn(providerAccountId, content, accessToken)
+    }
+  }
+}
+
+const scheduler = async (
+  scheduledPosts: any,
+  providerAccountId: String,
+  accessToken: String | null
+) => {
+  schedule.scheduleJob('* * * * *', async () => {
+    const currentTime = new Date()
+    console.log(
+      currentTime.toLocaleTimeString(),
+      '=======currentTime.getTime()'
+    )
+    scheduledPosts.forEach((item: any) => {
+      if (currentTime >= item.time) {
+        postOnLinkedIn(providerAccountId, item?.content, accessToken)
+      }
+    })
+  })
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json()
+
+    const userAccount = await db.account.findFirst({
+      where: { userId: body?.userId },
+    })
+
+    if (!userAccount) {
+      return 'Connect you linkedin account'
+    }
+
+    await scheduler(
+      scheduledPosts,
+      userAccount?.providerAccountId,
+      userAccount?.access_token
+    )
+
+    return NextResponse.json({ message: 'Post scheduled' }, { status: 200 })
+  } catch (error: any) {
+    return NextResponse.json({ error: 'Something went wrong' }, { status: 500 })
+  }
+}
