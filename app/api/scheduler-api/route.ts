acb8154fc61f8eadcc816ec8587f05a9b89f7e82
RicSala <ricsala86@gmail.com>
2024-04-09 17:16:17 +0200

    adding logs to webhook route

--- app/api/scheduler-api/route.ts
+++ app/api/scheduler-api/route.ts
@@ -6,6 +6,7 @@ import {
 } from '../../_actions/schedule-actions';
 import { TLinkedinPost, TScheduledPost } from '@/types/types';
 import { NextRequest, NextResponse } from 'next/server';
+import { differenceInMinutes } from 'date-fns';
 
 type NewTScheduledPost = TScheduledPost & {
     linkedinPost: TLinkedinPost;
@@ -22,6 +23,7 @@ export async function GET(req: NextRequest) {
         console.log('Time Now', now);
         const startOfDay = new Date();
         startOfDay.setHours(0, 0, 0, 0);
+        console.log('Start of the day', startOfDay);
 
         // finding the posts from the start of the day till end of day that are not published AND SHOULD BE PUBLISHED BY NOW
         let pendingToPublishPosts = (await db.scheduledPost.findMany({
@@ -30,14 +32,27 @@ export async function GET(req: NextRequest) {
             //         gte: startOfDay,
             //         lt: now,
             //     },
-            //     linkedinPost: {
-            //         published: false,
-            //     },
+            //     // linkedinPost: {
+            //     //     published: false,
+            //     // },
             // },
             include: {
                 linkedinPost: true,
             },
         })) as NewTScheduledPost[];
+
+        pendingToPublishPosts.forEach((post) => {
+            console.log('Post Date', post.date);
+            console.log('Now', now);
+            const isBeforeNow = post.date < now;
+            console.log('Is Before Now', isBeforeNow);
+            const isAfterStartOfDay = post.date > startOfDay;
+            console.log('Is After Start of Day', isAfterStartOfDay);
+
+            // fns to see the difference between two dates
+            const diff = differenceInMinutes(post.date, now);
+            console.log('Difference in minutes', diff);
+        });
 
         console.log('Posts to be published', pendingToPublishPosts);
         return NextResponse.json({ message: 'Scheduled' }, { status: 201 });
@@ -134,3 +149,104 @@ export async function GET(req: NextRequest) {
         );
     }
 }
+
+// const posts = [
+//     {
+//       id: '660800e72633403eac646e52',
+//       time: '12:00 AM',
+//       date: 2024-04-02T10:00:00.000Z,
+//       userId: '65fd6dbb1a20ce76da44b999',
+//       createdAt: 2024-03-30T12:09:11.139Z,
+//       updatedAt: 2024-03-30T12:09:11.139Z,
+//       linkedinPostId: null,
+//       linkedinPost: null
+//     },
+//     {
+//       id: '660803ca2633403eac646e53',
+//       time: '10:45 PM',
+//       date: 2024-04-01T08:45:00.000Z,
+//       userId: '65fd6dbb1a20ce76da44b999',
+//       createdAt: 2024-03-30T12:21:30.695Z,
+//       updatedAt: 2024-03-30T12:21:30.695Z,
+//       linkedinPostId: null,
+//       linkedinPost: null
+//     },
+//     {
+//       id: '660804d52633403eac646e57',
+//       time: '04:15 PM',
+//       date: 2024-04-01T02:15:00.000Z,
+//       userId: '65fd6dbb1a20ce76da44b999',
+//       createdAt: 2024-03-30T12:25:57.365Z,
+//       updatedAt: 2024-03-30T12:25:57.365Z,
+//       linkedinPostId: null,
+//       linkedinPost: null
+//     },
+//     {
+//       id: '660804f82633403eac646e58',
+//       time: '10:45 PM',
+//       date: 2024-04-01T08:45:00.000Z,
+//       userId: '65fd6dbb1a20ce76da44b999',
+//       createdAt: 2024-03-30T12:26:32.730Z,
+//       updatedAt: 2024-03-30T12:26:32.730Z,
+//       linkedinPostId: null,
+//       linkedinPost: null
+//     },
+//     {
+//       id: '660a7652adf23b88053f4ca7',
+//       time: '03:45 AM',
+//       date: 2024-04-01T01:45:00.000Z,
+//       userId: '65fd6dbb1a20ce76da44b999',
+//       createdAt: 2024-04-01T08:54:42.537Z,
+//       updatedAt: 2024-04-01T08:54:42.537Z,
+//       linkedinPostId: null,
+//       linkedinPost: null
+//     },
+//     {
+//       id: '660e84c1b2d05a9df5e0b323',
+//       time: '12:00 PM',
+//       date: 2024-04-04T10:00:00.000Z,
+//       userId: '660e7129b2d05a9df5e0b319',
+//       createdAt: 2024-04-04T10:45:21.815Z,
+//       updatedAt: 2024-04-04T10:45:21.815Z,
+//       linkedinPostId: null,
+//       linkedinPost: null
+//     },
+//     {
+//       id: '661552e5cbb66ec98d050a3e',
+//       time: '12:15 AM',
+//       date: 2024-04-09T00:15:00.000Z,
+//       userId: '6612fae819b46e0f9bb326d4',
+//       createdAt: 2024-04-09T14:38:29.979Z,
+//       updatedAt: 2024-04-09T14:38:29.979Z,
+//       linkedinPostId: '661548092f9881720f6326b8',
+//       linkedinPost: {
+//         id: '661548092f9881720f6326b8',
+//         createdAt: 2024-04-09T13:52:09.590Z,
+//         updatedAt: 2024-04-09T13:52:09.590Z,
+//         content: 'This is a test to see how it works',
+//         published: false,
+//         publishedAt: null,
+//         userId: '6612fae819b46e0f9bb326d4',
+//         author: [Object]
+//       }
+//     },
+//     {
+//       id: '661555db7da1ec3c9ad9f391',
+//       time: '12:15 AM',
+//       date: 2024-04-08T00:15:00.000Z,
+//       userId: '6612fae819b46e0f9bb326d4',
+//       createdAt: 2024-04-09T14:51:07.163Z,
+//       updatedAt: 2024-04-09T14:51:07.163Z,
+//       linkedinPostId: '66155589574784e7032fbee3',
+//       linkedinPost: {
+//         id: '66155589574784e7032fbee3',
+//         createdAt: 2024-04-09T14:49:45.610Z,
+//         updatedAt: 2024-04-09T14:49:45.610Z,
+//         content: 'test',
+//         published: false,
+//         publishedAt: null,
+//         userId: '6612fae819b46e0f9bb326d4',
+//         author: [Object]
+//       }
+//     }
+//   ]
