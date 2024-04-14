41c1d428a58bbdde46c3a5ac869294aa30b95c1d
RicSala <ricsala86@gmail.com>
2024-04-14 20:03:45 +0200

    improving

--- app/A-dev-ideas/const.ts
+++
@@ -1,1 +0,0 @@
-export const generateIdeasPrompt = `Genera un array de 5 ideas para escribir post en Linkedin sobre {topic}. Cada idea del array debe seguir estas instrucciones de formato {format_instructions}`;
