85a28c976831298c29f270bb37072b4d568b0fe0
RicSala <ricsala86@gmail.com>
2024-04-24 14:20:43 +0200

    create ai lib

---
+++ lib/aiClients.ts
@@ -0,0 +1,57 @@
+// TODO: to implement tracing and feedback from the user
+
+import { ChatOpenAI, DallEAPIWrapper } from '@langchain/openai';
+import { getAiModel } from './utils';
+import { OpenAIWhisperAudio } from 'langchain/document_loaders/fs/openai_whisper_audio';
+
+export const aiChat = (
+    use: 'ideas' | 'writter' | 'carousel',
+    streaming = false,
+    temperature = 0.8,
+    maxTokens = 3000
+    // TODO:
+    // callbacks: OpenAIStreamCallbacks = [] as OpenAIStreamCallbacks
+) => {
+    return new ChatOpenAI({
+        temperature,
+        modelName: getAiModel(use),
+        maxTokens,
+        streaming,
+        callbacks: [
+            {
+                handleLLMNewToken(token) {
+                    // console.log(token);
+                },
+            },
+        ],
+    });
+};
+
+// const imageGeneration = new DallEAPIWrapper({
+//     n: 1, // Default
+//     modelName: 'dall-e-3', // Default
+//     openAIApiKey: process.env.OPENAI_API_KEY, // Default
+//     quality: 'standard', // Default
+// });
+
+export const aiDraw = (
+    n = 1,
+    model = 'dall-e-3',
+    quality: 'standard' | 'hd' | undefined = 'standard'
+) => {
+    return new DallEAPIWrapper({
+        n,
+        modelName: model,
+        openAIApiKey: process.env.OPENAI_API_KEY,
+        quality,
+    });
+};
+
+export const aiTranscribe = (filePath: string) => {
+    return new OpenAIWhisperAudio(filePath, {
+        clientOptions: {
+            // TODO: How can we add parameters to the client?
+            // response_format: 'vtt',
+        },
+    });
+};
