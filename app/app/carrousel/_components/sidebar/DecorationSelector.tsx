25fa56dbabb2be2ee296b5459408424131c04484
RicSala <ricsala86@gmail.com>
2024-04-29 18:16:00 +0200

    refactor

--- app/app/carrousel/_components/sidebar/DecorationSelector.tsx
+++ app/app/carrousel/_components/sidebar/DecorationSelector.tsx
@@ -40,7 +40,7 @@ export function DecorationSelector({
                 'HorizontalGradient',
                 'Prism',
                 'Starts',
-                'Paper',
+                // 'Paper',
             ].includes(design)
         )
             return true;
